4f99afe4a12321a6f16ef99432f30494ed244c40
ledrey <dledrey@yandex.com>
2022-07-30 03:39:02 +0300

    fix: alter init files and settings

--- dist/config/dataSource.d.ts
+++
@@ -1,5 +0,0 @@
-import { DataSource } from 'typeorm';
-export declare const dataBase: {
-    provide: string;
-    useFactory: () => Promise<DataSource>;
-}[];
--- dist/shared/entities/users.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Record } from './records.entity';
-export declare class User {
-    id: number;
-    name: Text;
-    location: Text;
-    createdAt: Date;
-    updatedAt: Date;
-    records: Record[];
-}
